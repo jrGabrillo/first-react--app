359e0a0dad513506368eb54e19c3bc576542824f
Rufo N. Gabrillo Jr <rufo.gabrillo@gmail.com>
2017-11-03 10:57:49 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function hello(user){
-	return `${user.name}`;
 
-}
-
-let user = {
-	name: "Rufo N. Gabrillo Jr."
-}
-
-let App = <h1>{hello()}</h1>;
+let App = <h1>Hello World</h1>;
 
 
 ReactDOM.render(App, document.getElementById('root'));
