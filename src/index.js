fd82acfeb3032882eb3cf96a6fca8d1b923bcd67
Rufo Gabrillo <rufo.gabrillo@gmail.com>
2017-11-08 08:27:21 +0800

    Update;

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-<<<<<<< HEAD
-let a = 0;
 function clock(){
-	a++;
-	let element = <h1>{a}</h1>;
-=======
-function clock(){
-	let element = <h1>{new Date().toLocaleTimeString()}</h1>;
->>>>>>> 82e5b9c29b5806aa5757895e5b8ee3adf21c9bc0
-	ReactDOM.render(element, document.getElementById('root'));
-}
+	let time = `${new Date().toLocaleTimeString()}`;
+	ReactDOM.render(time,document.getElementById('root'));	
+	setTimeout(function(){
+		clock();	
+	},1000)
+};
 
-setInterval(function(){
-	clock();
-<<<<<<< HEAD
-},1000);
-=======
-},1000);
->>>>>>> 82e5b9c29b5806aa5757895e5b8ee3adf21c9bc0
+let btn = <div>
+			<button onClick={clock}>Click Me</button>
+			</div>;
+
+ReactDOM.render(btn,document.getElementById('root'));
